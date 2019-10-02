eb59ba376ab5c7571378ef1089e66fe8c26a5a11
anant <anantmal007@gmail.com>
2019-10-02 18:01:08 +0530

    splash and drawer layout with logout

--- app/src/main/java/com/example/demoapp/HomeActivity.java
+++ app/src/main/java/com/example/demoapp/HomeActivity.java
@@ -6,21 +6,47 @@ import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 import androidx.drawerlayout.widget.DrawerLayout;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.MenuItem;
+
+import com.google.android.material.navigation.NavigationView;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class HomeActivity extends AppCompatActivity {
     private DrawerLayout mDrawerLayout;
     private ActionBarDrawerToggle mToggle;
 
+    FirebaseAuth mAuth;
+    FirebaseAuth.AuthStateListener mAuthListener;
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        mAuth.addAuthStateListener(mAuthListener);
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
         mDrawerLayout=(DrawerLayout)findViewById(R.id.drawer);
+        NavigationView navigationView=(NavigationView)findViewById(R.id.navigationView);
         mToggle=new ActionBarDrawerToggle(this,mDrawerLayout,R.string.open,R.string.close);
+
+        mAuth=FirebaseAuth.getInstance();
+        mAuthListener=new FirebaseAuth.AuthStateListener() {
+            @Override
+            public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
+                if(firebaseAuth.getCurrentUser()==null){
+                    startActivity(new Intent(HomeActivity.this,SignInActivity.class));
+                }
+            }
+        };
+
         mDrawerLayout.addDrawerListener(mToggle);
         mToggle.syncState();
 
@@ -30,7 +56,45 @@ public class HomeActivity extends AppCompatActivity {
         ActionBar actionBar = getSupportActionBar();
         actionBar.setDisplayHomeAsUpEnabled(true);
         actionBar.setHomeAsUpIndicator(R.drawable.ic_navigation_drawer);
+        setUpDrawerContent(navigationView);
 
+    }
+    public void selectItemDrawer(MenuItem menuItem){
+        Fragment myfragment=null;
+        Class fragmentClass;
+        switch (menuItem.getItemId()){
+            case R.id.about:
+                fragmentClass=About.class;
+                break;
+                default:
+                    fragmentClass=About.class;
+                    break;
+        }
+        try{
+            myfragment=(Fragment)fragmentClass.newInstance();
+        }
+        catch (Exception e){
+            e.printStackTrace();
+        }
+        FragmentManager fragmentManager=getSupportFragmentManager();
+        fragmentManager.beginTransaction().replace(R.id.l_layout,myfragment).commit();
+        menuItem.setChecked(true);
+        setTitle(menuItem.getTitle());
+        mDrawerLayout.closeDrawers();
+
+    }
+
+    private void setUpDrawerContent(NavigationView navigationView){
+        navigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+                if(menuItem.getItemId()==R.id.logout){
+                    mAuth.signOut();
+                }
+                selectItemDrawer(menuItem);
+                return true;
+            }
+        });
     }
 
     @Override
